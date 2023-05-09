f414a53ec6618b369ae62e95271fa327a3f3e36b
Ouerghie Houssem <126062032+OuerghieHoussem1@users.noreply.github.com>
2023-05-09 22:56:55 +0100

    arrow

--- src/components/PassCardPage.js
+++ src/components/PassCardPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {  useNavigate } from 'react-router-dom'
 import cardImage from "../carte pesonnelle.png"
+import {AiOutlineArrowRight} from "react-icons/ai"
 export default function PassCardPage() {
   const Navigate = useNavigate()
 
@@ -11,8 +12,11 @@ export default function PassCardPage() {
   return (    
     <div>
       <div className="h-screen w-screen justify-around items-center bg-[url('/src/background.png')] flex flex-row">
-          <h1 onClick={CheckCard} className="text-white text-8xl font-bold font-sans ">Scanner votre<br/> carte</h1>
-          <img src={cardImage} alt='' className=' rounded-2xl aspect-video w-96 border-[3px] animate-bounce '/>
+          <h1 onClick={CheckCard} className=" text-white text-8xl font-bold font-sans ">SCANNER VOTRE<br/> CARTE</h1>
+          <div className='flex flex-col gap-4 mt-60'>
+            <img src={cardImage} alt='' className=' rounded-2xl aspect-video w-96 border-[3px] animate-bounce '/>
+            <AiOutlineArrowRight size={250} color='white' className='w-full'/>
+          </div>
       </div>
     </div>
     
