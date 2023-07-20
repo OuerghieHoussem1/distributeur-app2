b6620e76e5d33f21240aa9d0927ccaf83523a4d9
Ouerghie Houssem <126062032+OuerghieHoussem1@users.noreply.github.com>
2023-07-20 16:46:27 +0100

    Update sensor2.py

--- Server/scripts/sensor2.py
+++ Server/scripts/sensor2.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
-""" from luma.core.interface.serial import spi, noop """
+from luma.core.interface.serial import spi, noop
 """ 
 from luma.led_matrix.device import max7219
 
@@ -10,7 +10,7 @@ from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message, textsize
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT """
 
-""" serial = spi(port=0, device=0, gpio=noop()) """
+serial = spi(port=0, device=0, gpio=noop())
 """ device = max7219(serial, cascaded=4, block_orientation=-90) """
 id = 1
 print ("Untuk menampilkan ID RFID pada LED")
