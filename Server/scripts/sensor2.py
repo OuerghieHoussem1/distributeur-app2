cbfaaf4eaa3131242ff7d9a4ddd86de8f74a7cea
Ouerghie Houssem <126062032+OuerghieHoussem1@users.noreply.github.com>
2023-07-26 19:28:20 +0100

    Update sensor2.py

--- Server/scripts/sensor2.py
+++ Server/scripts/sensor2.py
@@ -1,6 +1,7 @@
 import evdev
 from evdev import categorize, ecodes
 import requests
+import subprocess
 
 
 class Device():
@@ -28,34 +29,37 @@ class Device():
 
     @classmethod
     def run(cls):
-        device = cls.connect()
-        container = []
         try:
-            device.grab()
-            # bind the device to the script
-            print("RFID scanner is ready....")
-            print("Press Control + c to quit.")
-            for event in device.read_loop():
-                    # enter into an endeless read-loop
-                    if event.type == ecodes.EV_KEY and event.value == 1:
-                        digit = evdev.ecodes.KEY[event.code]
-                        if digit == 'KEY_ENTER':
-                            # create and dump the tag
-                            tag = "".join(i.strip('KEY_') for i in container)
-                            print(tag)
-                            val = requests.post(
-                            "http://localhost:5000/checkCard", 
-                            data=None,
-                            json={'cardId':tag}
-                            )
-                            container = []
-                        else:
-                            container.append(digit)
+            device = cls.connect()
+            container = []
+            try:
+                device.grab()
+                # bind the device to the script
+                print("RFID scanner is ready....")
+                print("Press Control + c to quit.")
+                for event in device.read_loop():
+                        # enter into an endeless read-loop
+                        if event.type == ecodes.EV_KEY and event.value == 1:
+                            digit = evdev.ecodes.KEY[event.code]
+                            if digit == 'KEY_ENTER':
+                                # create and dump the tag
+                                tag = "".join(i.strip('KEY_') for i in container)
+                                print(tag)
+                                val = requests.post(
+                                "http://localhost:5000/checkCard", 
+                                data=None,
+                                json={'cardId':tag}
+                                )
+                                container = []
+                            else:
+                                container.append(digit)
 
-        except Exception as e:
-            # catch all exceptions to be able release the device
-            print(e)
-            device.ungrab()
-            print('Quitting.')
+            except Exception as e:
+                # catch all exceptions to be able release the device
+                print(e)
+                device.ungrab()
+                print('Quitting.')
+        except :
+            subprocess.Popen('python3 /root/distributeur-app2/Server/scripts/sensor2.py', shell=True)
 
 Device.run()
